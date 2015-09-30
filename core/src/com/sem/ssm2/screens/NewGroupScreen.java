791a264392e62f669f8bd5ab73a798d278f5cff7
Jurgen van Schagen <J.T.vanSchagen@student.tudelft.nl>
2015-10-01 00:29:52 +0200

    Added checkbox

--- core/src/com/sem/ssm2/screens/NewGroupScreen.java
+++ core/src/com/sem/ssm2/screens/NewGroupScreen.java
@@ -87,11 +87,38 @@ public class NewGroupScreen extends BaseMenuScreen {
         );
 
 
-        group.add(new Label("Group name:", labelStyle))
+        pixmap = new Pixmap((int)(60 * assets.getRatio()), (int)(60 * assets.getRatio()), Pixmap.Format.RGBA8888);
+        pixmap.setColor(Color.RED);
+        pixmap.fill();
+        pixmap.setColor(Color.GREEN);
+        pixmap.fillRectangle(2, 2, pixmap.getWidth() - 4, pixmap.getHeight() - 4);
+        TextureRegionDrawable uncheckedDrawable = new TextureRegionDrawable(new TextureRegion(new Texture(pixmap)));
+
+        pixmap.setColor(Color.ORANGE);
+        pixmap.fillRectangle(2, 2, pixmap.getWidth() - 4, pixmap.getHeight() - 4);
+        TextureRegionDrawable checkedDrawable = new TextureRegionDrawable(new TextureRegion(new Texture(pixmap)));
+
+        CheckBox.CheckBoxStyle checkBoxStyle = new CheckBox.CheckBoxStyle(
+                uncheckedDrawable,
+                checkedDrawable,
+                assets.get("white_buttonFont", BitmapFont.class),
+                Color.TEAL
+        );
+
+        pixmap.dispose();
+
+
+        group.add(new Label("Group name:", labelStyle)).colspan(2)
                 .pad(0, Gdx.graphics.getWidth() * 0.15f, 0, Gdx.graphics.getWidth() * 0.15f).fillX().expandX();
         group.row();
         TextField textField = new TextField("text", textFieldStyle);
-        group.add(textField).width(0.7f * Gdx.graphics.getWidth());
+        group.add(textField).width(0.7f * Gdx.graphics.getWidth()).colspan(2);
+        group.row();
+        group.add(new Label("Public:", labelStyle))
+                .padLeft(Gdx.graphics.getWidth() * 0.15f).fillX().expandX();
+        group.add(new CheckBox("", checkBoxStyle)).padRight(Gdx.graphics.getWidth() * 0.15f);
+        group.row();
+        group.add(new TextButton("Create", buttonStyle)).colspan(2);
         group.row();
 //
 //        group.addActor(new Label("Group name:", labelStyle));
